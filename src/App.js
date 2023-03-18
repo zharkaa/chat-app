28162ef51efb090dd893194b5320b9ceca9252d2
Patrick Walujan <patrickwalujan.dev@gmail.com>
2023-03-18 23:53:09 +0800

    add login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import "./style.scss";
 
 function App() {
-  return (
-    <div className="formContainer">
-      <div className="formWrapper">
-        <span className="logo">Chat App</span>
-        <span className="title">Register</span>
-        <form>
-          <input type="text" placeholder="Display Name" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="file" />
-          <button>Sign Up</button>
-          <p>You do have an account ? Login</p>
-        </form>
-      </div>
-    </div>
-  );
+  return <Login />;
 }
 
 export default App;

0ddeec311244d746c5adaa2dae0dc8923f529f08
Patrick Walujan <patrickwalujan.dev@gmail.com>
2023-03-18 22:51:56 +0800

    add sign up UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import Register from "./pages/Register";
+
 function App() {
   return (
-    <div>
-      Hello world!
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Chat App</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="text" placeholder="Display Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input type="file" />
+          <p>You do have an account ? Login</p>
+        </form>
+      </div>
     </div>
   );
 }

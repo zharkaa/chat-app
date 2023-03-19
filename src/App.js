7505bc76109650a55706d9e6f83004227a7e9453
Patrick Walujan <patrickwalujan.dev@gmail.com>
2023-03-19 19:52:26 +0800

    add style for home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Home from "./pages/Home";
+
 import "./style.scss";
 
 function App() {
-  return <Login />;
+  return <Home />;
 }
 
 export default App;

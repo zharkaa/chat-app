2174ffb8e109f022ecfe26ba2e41ee30fdc11506
Patrick Walujan <patrickwalujan.dev@gmail.com>
2023-03-20 23:34:53 +0800

    add topbar and styling

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import Cam from "../img/cam.png";
+import Add from "../img/add.png";
+import More from "../img/more.png";
 
 const Chat = () => {
   return (
-    <div className='chat'>
-      
+    <div className="chat">
+      <div className="chatInfo">
+        <span>User 3</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
